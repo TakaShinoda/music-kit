5c8f364d42d9b54da7ad11c7cf2e2590bf237b21
Takahiro <tkhr_621@docomo.ne.jp>
2021-03-14 18:53:13 +0900

    import conponents

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,9 +1,11 @@
 import Layout from '../components/Layout'
+import { MusicSearch } from '../components/MusicSearch'
 
 const SearchPage = () => (
   <Layout title="Search">
-    <h1>About</h1>
-    <p>This is the about page</p>
+    <h1>Search</h1>
+    <p>This is the search page</p>
+    <MusicSearch />
   </Layout>
 )
 

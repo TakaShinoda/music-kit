401ff0342e86f429706b909b55f24c085cac5389
Takahiro <tkhr_621@docomo.ne.jp>
2021-03-15 00:01:21 +0900

    fix

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Apple Musicの音楽を再生するWebサイト作成中
 
 ## ⛏開発中の画面
-![スクリーンショット 2021-03-13 14 18 11](https://user-images.githubusercontent.com/45593212/111020185-5cb5fc00-8407-11eb-86aa-6f598468b737.png)
+![スクリーンショット 2021-03-14 19 58 44](https://user-images.githubusercontent.com/45593212/111065941-bdc0fb00-84ff-11eb-9eb5-1ace9181b192.png)
 
 
 ## 使用技術
--- components/TodaysHit.tsx
+++ components/TodaysHit.tsx
@@ -3,19 +3,29 @@ import React, { FC } from 'react'
 export const TodaysHit: FC = () => {
   return (
     <div>
+      {/* auth */}
+      <div>
+        <button data-apple-music-authorize>sign in</button>
+        <button data-apple-music-unauthorize>sign out</button>
+      </div>
+
       {/* To set a queue using a playlist identifier in the Apple Music API */}
-      <button data-apple-music-set-queue="pl.f4d106fed2bd41149aaacabb233eb5eb">
+      {/* <button data-apple-music-set-queue="pl.f4d106fed2bd41149aaacabb233eb5eb">
         Today's hits
+      </button> */}
+
+      <button data-apple-music-set-queue="https://music.apple.com/jp/playlist/%E3%83%88%E3%83%83%E3%83%97100-%E6%97%A5%E6%9C%AC/pl.043a2c9876114d95a4659988497567be">
+        Japan Top 100
       </button>
 
       {/* Dynamite */}
       {/* <button data-apple-music-set-queue="1528831887">再生</button> */}
 
       {/* Add Media Playback Controls */}
-      <button data-apple-music-pause></button>
-      <button data-apple-music-play></button>
-      <button data-apple-music-skip-to-next-item></button>
-      <button data-apple-music-skip-to-previous-item></button>
+      <button data-apple-music-pause>pause</button>
+      <button data-apple-music-play>play</button>
+      <button data-apple-music-skip-to-next-item>next</button>
+      <button data-apple-music-skip-to-previous-item>prev</button>
 
       {/* nowPlayingItem.artworkURL */}
       <p>
@@ -27,13 +37,19 @@ export const TodaysHit: FC = () => {
       </p>
 
       {/* Default nowPlayingItem.info */}
-      <p data-apple-music-now-playing></p>
+      {/* <p data-apple-music-now-playing></p> */}
+      <p data-apple-music-now-playing="playlistName"></p>
+      <p data-apple-music-now-playing="title"></p>
+      <p data-apple-music-now-playing="artistName"></p>
 
       {/* Add Current Playback Information */}
+      {/* 曲時間、再生時間、再生進行状況 */}
       <p>
-        <time id="apple-music-current-playback-duration"></time>
-        <time id="apple-music-current-playback-time"></time>
-        <span id="apple-music-current-playback-progress"></span>
+        曲時間: <time id="apple-music-current-playback-duration"></time>
+        <br />
+        再生時間: <time id="apple-music-current-playback-time"></time>
+        <br />
+        進行状況<span id="apple-music-current-playback-progress"></span>
       </p>
     </div>
   )
